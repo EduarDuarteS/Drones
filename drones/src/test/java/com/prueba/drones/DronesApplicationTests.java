8798f90a3f3b55113fb591436b73b3442d77ec00
eduardduarte <eduard.duarte.s@gmail.com>
2023-03-28 19:17:45 -0500

    init

---
+++ drones/src/test/java/com/prueba/drones/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
