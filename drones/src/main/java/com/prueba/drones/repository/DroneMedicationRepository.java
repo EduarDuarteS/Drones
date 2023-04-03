cca339b674a7af1d8e09ca8d1042fdf161303a4e
eduardduarte <eduard.duarte.s@gmail.com>
2023-04-02 20:41:14 -0500

    feat: add Query for fid with medications

--- drones/src/main/java/com/prueba/drones/repository/DroneMedicationRepository.java
+++ drones/src/main/java/com/prueba/drones/repository/DroneMedicationRepository.java
@@ -1,9 +1,18 @@
 package com.prueba.drones.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
 import com.prueba.drones.model.DroneMedication;
 
 @Component
 public interface DroneMedicationRepository extends JpaRepository<DroneMedication, Long> {
+    List<DroneMedication> findByDroneSerialNumber(String serialNumber);
+
+    @Query("SELECT dm FROM DroneMedication dm JOIN FETCH dm.medications WHERE dm.drone.serialNumber = :serialNumber")
+    DroneMedication findByDroneSerialNumberWithMedications(@Param("serialNumber") String serialNumber);
 }
